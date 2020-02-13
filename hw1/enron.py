c07a55ae21ec9f438bb90b43dffe72e38aa7e7a7
jzh <zhj199305@gmail.com>
2020-02-12 21:19:45 -0500

    clean

--- hw1/enron.py
+++ hw1/enron.py
@@ -1,19 +1,11 @@
 import scipy.io
-import snap
-import networkx as nx
 import numpy as np
-
 
 #Problem5
 
 #load data
 data = scipy.io.loadmat("./Enron/Y.mat")['Y']
 employees = scipy.io.loadmat("./Enron/employees.mat")['employees']
-
-
-for i in range(44):
-    if(np.amin(data[:, :, i])!=0):
-        print(i)
 
 # A) aggregate matrixs and map its element to {0, 1}
 A = np.where(np.sum(data, axis = 2) > 0, 1, 0)
@@ -24,9 +16,9 @@ for i in range(184):
 
 # G(V,E)
 # number of vertex
-V = len(A);
+V = len(A)
 # a set of ordered pair of vertices, in other word, directed arcs
-E = set();
+E = set()
 for i in range(V):
     for j in range(V):
         if A[i][j] == 1:
@@ -34,7 +26,7 @@ for i in range(V):
 
 # B) compute the number of directed edges
 d_edges = len(E)
-print(d_edges)
+print("# directed edges:", d_edges)
 
 # C) D) compute the number of undirected edges and mutual arcs
 # ud_edges = d_edges - mut_edges
@@ -44,47 +36,50 @@ for i, j in E:
         mut_edges += 1
 mut_edges //= 2
 ud_edges = d_edges - mut_edges
-print(ud_edges)
-print(mut_edges)
+print("# undirected edges:", ud_edges)
+print("# mutual arcs:",mut_edges)
 
 # E)
 indegree_0_ids = [] 
 for i in range(V):
     if np.sum(A[:, i])==0:
         indegree_0_ids.append(i)
-print(len(indegree_0_ids))
-
+print("# degree_in=0:", len(indegree_0_ids))
 indegree_0_names = []
 for e in indegree_0_ids:
-    indegree_0_names.append(employees[i])
-
+    indegree_0_names.append(employees[e][0][0])
+print(indegree_0_ids)
+print("employees:", indegree_0_names)
 
 # F)
 outdegree_0_ids = []
 for i in range(V):
     if np.sum(A[i, :])==0:
         outdegree_0_ids.append(i)
-print(len(outdegree_0_ids))
+print("# degree_out=0:", len(outdegree_0_ids))
 
 outdegree_0_names = []
 for e in outdegree_0_ids:
-    outdegree_0_names.append(employees[i])
+    outdegree_0_names.append(employees[e][0][0])
+print(outdegree_0_ids)
+print("employees:", outdegree_0_names)
+
 
 # G)
 employee_contacted_larger_than_30 = 0
 for v in range(V):
     if np.sum(A[:, v]) >= 30:
         employee_contacted_larger_than_30 += 1
-print(employee_contacted_larger_than_30)
+print("# connected >= 30:", employee_contacted_larger_than_30)
 
 # H)
 employee_contacting_larger_than_30 = 0
 for v in range(V):
     if np.sum(A[v,:]) >= 30:
         employee_contacting_larger_than_30 += 1
-print(employee_contacting_larger_than_30)
+print("# connecting >= 30:", employee_contacting_larger_than_30)
 
 # I)
 A3 = A.dot(A.dot(A))
 triangle_count = np.trace(A3) / 3
-print(triangle_count)
+print("# triangle:",triangle_count)
