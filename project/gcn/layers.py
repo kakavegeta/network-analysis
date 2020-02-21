33ee968614bd2cc74c69c58f5296a92432853b5a
jzh <zhj199305@gmail.com>
2020-02-20 22:39:05 -0500

    gcn

---
+++ project/gcn/layers.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F 
+from torch.nn.parameter import Parameter
+from torch.nn.modules.module import Module
+import numpy as numpy
+import math
+
+class GraphConv(Module):
+    def __init__(self):
+        pass
+
+    def reset_parameter_uniform(self):
+        pass
---
+++ project/gcn/models.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F 
+
+class GCN(nn.Module):
+    def __init__(self, nfeat, nhidd, nclass):
+        pass
+
+    def forward(self, x):
+        pass
+
